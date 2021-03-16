8b966d6126e64dd9392dc1b2404c9f16bfc25bab
Brenda Catalina Barahona Pinilla <bc.barahona@uniandes.edu.co>
2021-03-15 23:14:29 -0500

    menu, registro e imagenes

--- frontend/FrontInventario/src/app/app.module.ts
+++ frontend/FrontInventario/src/app/app.module.ts
@@ -1,35 +1,42 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 // Igx
-import { IgxListModule } from "igniteui-angular";
+import { IgxListModule } from 'igniteui-angular';
 // Registro
 import { RegistroModule } from './registro/registro.module';
 import { RegistroPrincipalComponent } from './registro/registro-principal/registro-principal.component';
 // Plan de compra
 import { PlanCompraModule } from './planCompra/planCompra.module';
 import { PlanCompraPrincipalComponent } from './planCompra/planCompra-principal/planCompra-principal.component';
-
+//Registro de usuario
+import { Registro_usuarioModule } from './registro_usuario/registro_usuario.module';
+import { Registro_usuarioComponent } from './registro_usuario/registro_usuario.component';
+//menu de opciones
+import { Menu_opcionesModule } from './menu_opciones/menu_opciones.module';
+import { Menu_opcionesComponent } from './menu_opciones/menu_opciones.component';
 @NgModule({
-  declarations: [
-  AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RegistroModule,
     RouterModule.forRoot([
       { path: 'registro', component: RegistroPrincipalComponent },
       { path: 'planCompra', component: PlanCompraPrincipalComponent },
+      { path: 'registroUsuario', component: Registro_usuarioComponent },
+      { path: 'menu', component: Menu_opcionesComponent },
     ]),
     BrowserAnimationsModule,
     FormsModule,
-    IgxListModule
+    IgxListModule,
+    Registro_usuarioModule,
+    Menu_opcionesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: []
+  schemas: [],
 })
 export class AppModule {}
---
+++ frontend/FrontInventario/src/app/registro_usuario/registro_usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro_usuario',
+  templateUrl: './registro_usuario.component.html',
+  styleUrls: ['./registro_usuario.component.css'],
+})
+export class Registro_usuarioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
