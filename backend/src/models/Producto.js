dea90d9a1a533293e48dd371a6a437d3673fc431
Alvaro Plata <ad.plata@uniandes.edu.co>
2021-04-22 16:20:26 -0500

    Registrar Producto Form Funcionando

--- backend/src/models/Producto.js
+++ backend/src/models/Producto.js
@@ -5,16 +5,17 @@ const app = require("../app")
 const productoSchema = new Schema(
     {
         nombre: {type: String, required: true},
-        id: {type: String, required: true},
         descripcion: {type: String, required: true},
         proveedor: {type: String, required: true},
-        responsable: {type: String, required: true},
-        ubicacion: {type: String, required: true},
-        estado: {type: String, required: true},
-        susbsistemaQueLoNecesita: {type: String, required: true},
-        comentariosExtra: {type: String, required: true},
-        disponibleParaUso: {type: Number, required: true},
-        disponibleParaArreglo: {type: Number, required: true},
+        //Podemos poner las familias en un enum
+        familia: {type: String, required: true},
+        //Podemos poner las ubicaciones posibles en un enum
+        ubicacionEnCubiculo: {type: String, required: true},
+        cantidadTotal: {type: Number, required: true},
+        cantidadDisponiblesParaUso: {type: Number, required: true},
+        cantidadDisponiblesParaArreglo: {type: Number, required: true},
+        cantidadEnUso: {type: Number, required: true},
+        cantidadEnArreglo: {type: Number, required: true},
         fotoPath: {type: String},
     },
     {
--- frontend/FrontInventario/src/app/services/producto/producto.service.ts
+++ frontend/FrontInventario/src/app/services/producto/producto.service.ts
@@ -7,7 +7,7 @@ import {Producto} from '../../models/producto';
 })
 export class ProductoService {
 
-  URL_API = 'http://localhost:4000/api/products'
+  URL_API = 'http://localhost:4000/api/productos'
 
   constructor(private httpClient : HttpClient) { }
 
@@ -15,7 +15,23 @@ export class ProductoService {
     return this.httpClient.get<Producto>(this.URL_API);
   }
 
-  createProduct(producto: Producto){
-    return this.httpClient.post(this.URL_API, producto);
+
+  //Se hizo así y no con un método normal para hacer un POST de producto, porque por alguna razón no estaba enviando bien la imagen.
+  createProductByHtml(nombre: string, proveedor: string, familia: string, ubicacionEnCubiculo: string, descripcion: string, cantidadTotal: string, image: File){
+    const fd = new FormData();
+
+    fd.append("nombre", nombre);
+    fd.append("proveedor", proveedor);
+    fd.append("familia", familia);
+    fd.append("ubicacionEnCubiculo", ubicacionEnCubiculo);
+    fd.append("descripcion", descripcion);
+    fd.append("cantidadTotal", cantidadTotal);
+    fd.append("cantidadDisponiblesParaUso", "0");
+    fd.append("cantidadDisponiblesParaArreglo", "0");
+    fd.append("cantidadEnUso", "0");
+    fd.append("cantidadEnArreglo", "0");
+    fd.append("image", image);
+
+    return this.httpClient.post(this.URL_API, fd);
   }
 }
