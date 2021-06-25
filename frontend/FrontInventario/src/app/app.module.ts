dbb05ba7bf0a171708aed6916225caf8ff7f95f4
Alvaro Plata <ad.plata@uniandes.edu.co>
2021-06-24 21:04:44 -0500

    cosas

--- frontend/FrontInventario/src/app/app.module.ts
+++ frontend/FrontInventario/src/app/app.module.ts
@@ -10,15 +10,6 @@ import { IgxListModule } from 'igniteui-angular';
 // Registro de Producto
 import { RegistroProductoModule } from './modules/registro-producto/registro-producto.module';
 import { RegistroProductoPrincipalComponent } from './modules/registro-producto/registro-producto-principal/registro-producto-principal.component';
-// Mensajes
-import { MensajesModule } from './modules/mensajes/mensajes.module';
-import { MensajesPrincipalComponent } from './modules/mensajes/mensajes-principal/mensajes-principal.component';
-// Plan de compra
-import { PlanCompraModule } from './modules/planCompra/planCompra.module';
-import { PlanCompraPrincipalComponent } from './modules/planCompra/planCompra-principal/planCompra-principal.component';
-import { PlanCompraNuevoComponent } from './modules/planCompra/planCompra-nuevo/planCompra-nuevo.component';
-import { PlanCompraFormularioComponent } from './modules/planCompra/planCompra-formulario/planCompra-formulario.component';
-import { PlanCompraProductosComponent } from './modules/planCompra/planCompra-productos/planCompra-productos.component';
 //menu de opciones
 import { Menu_opcionesModule } from './modules/menu_opciones/menu_opciones.module';
 import { Menu_opcionesComponent } from './modules/menu_opciones/menu_opciones.component';
@@ -28,9 +19,6 @@ import { IngresoUsuarioComponent } from './modules/ingreso-usuario/ingreso-usuar
 //Historial
 import { HistorialComponent } from './modules/historial/historial.component';
 import { UsuarioGuard } from './guard/usuario.guard';
-//Catalogo
-import { CatalogoComponent } from './modules/catalogo/catalogo.component';
-import { CatalogoModule } from './modules/catalogo/catalogo.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -38,29 +26,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     RegistroProductoModule,
-    MensajesModule,
-    PlanCompraModule,
-    CatalogoModule,
     HttpClientModule,
-    RouterModule.forRoot([ /**/
+    RouterModule.forRoot([
       { path: 'registroProducto', component: RegistroProductoPrincipalComponent, canActivate:[UsuarioGuard] },
-      { path: 'mensajes', component: MensajesPrincipalComponent, canActivate:[UsuarioGuard] },
-      { path: 'planCompra', component: PlanCompraPrincipalComponent, canActivate:[UsuarioGuard] },
-      { path: 'planCompraNuevo', component: PlanCompraNuevoComponent, canActivate:[UsuarioGuard] },
-      { path: 'planCompraEstado', component: PlanCompraFormularioComponent, canActivate:[UsuarioGuard] },
       { path: 'menu', component: Menu_opcionesComponent, canActivate:[UsuarioGuard] },
       { path: 'ingreso-usuario', component: IngresoUsuarioComponent},
       { path: '', pathMatch:'full', redirectTo: 'ingreso-usuario'},
       { path: 'historial', component: HistorialComponent, canActivate:[UsuarioGuard]},
-      { path: 'catalogo', component: CatalogoComponent, canActivate:[UsuarioGuard] },
-      /*
-      { path: 'registroProducto', component: RegistroProductoPrincipalComponent },
-      { path: 'planCompra', component: PlanCompraPrincipalComponent },
-      { path: 'menu', component: Menu_opcionesComponent },
-      { path: 'catalogo', component: CatalogoComponent },
-      { path: 'ingreso-usuario', component: IngresoUsuarioComponent},
-      { path: '', pathMatch:'full', redirectTo: 'ingreso-usuario'},
-      { path: 'historial', component: HistorialComponent}*/
     ]),
     BrowserAnimationsModule,
     FormsModule,
