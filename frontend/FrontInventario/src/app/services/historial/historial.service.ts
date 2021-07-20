e09f7df367db50c762a1f31fa3393247f77e1f0d
Alvaro Plata <ad.plata@uniandes.edu.co>
2021-07-20 18:11:35 -0500

    Revert "Cambio de environment por environment.prod"
    This reverts commit f231f4cb781ec4b55762776806869bd20f651561.

--- frontend/FrontInventario/src/app/services/historial/historial.service.ts
+++ frontend/FrontInventario/src/app/services/historial/historial.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Historial } from "../../models/historial";
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
